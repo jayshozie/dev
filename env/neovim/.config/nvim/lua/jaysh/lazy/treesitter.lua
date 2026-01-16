8e74b7edef521366af50d1fec04d0d5b4d174b35
jayshozie <143549527+jayshozie@users.noreply.github.com>
2026-01-16 18:33:29 +0300

    wip: dev commit

--- env/neovim/.config/nvim/lua/jaysh/lazy/treesitter.lua
+++ env/neovim/.config/nvim/lua/jaysh/lazy/treesitter.lua
@@ -1,44 +1,19 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  lazy = false,
-  build = ':TSUpdate'
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+        require("nvim-treesitter").setup({
+            ensure_installed = {
+                "c", "lua", "vim", "vimdoc", "query", "cpp", "cmake", "make",
+                "python", "markdown", "latex", "asm", "bash",
+            },
+            sync_install = true,
+            auto_install = true,
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
+        })
+    end,
 }
--- return {
---     -- PLUGIN 1: Treesitter (The Engine)
---     {
---         "nvim-treesitter/nvim-treesitter",
---         build = ":TSUpdate",
---         lazy = false, -- CHANGE THIS: Force load immediately to fix the path issue
---         -- event = { "BufReadPre", "BufNewFile" }, -- Comment this out for now
---         opts = {
---             ensure_installed = {
---                 "c", "lua", "vim", "vimdoc", "query", "cpp", "cmake", "make",
---                 "python", "markdown", "latex", "asm", "bash",
---             },
---             sync_install = false,
---             auto_install = true,
---             highlight = {
---                 enable = true,
---                 additional_vim_regex_highlighting = false,
---             },
---         },
---         config = function(_, opts)
---             require("nvim-treesitter.configs").setup(opts)
--- 
---             vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
---                 pattern = { "*.service" },
---                 callback = function()
---                     vim.cmd("set filetype=ini")
---                 end,
---             })
---         end,
---     },
--- 
---     -- PLUGIN 2: Context
---     {
---         "nvim-treesitter/nvim-treesitter-context",
---         dependencies = { "nvim-treesitter/nvim-treesitter" },
---         lazy = false, -- Force load this too
---         config = true,
---     },
--- }
